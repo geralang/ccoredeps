fee812ea26158853bffcf7b5a885eafb3c07cdfe
TypeSafeSchwalbe <78918080+TypeSafeSchwalbe@users.noreply.github.com>
2024-02-23 19:04:26 +0200

    Fix 'integer constant is so large that it is unsigned'

--- gera.h
+++ gera.h
@@ -23,7 +23,7 @@
     } \
     d_name[s_name.length_bytes] = '\0';
 
-#define GERA_INT_MIN -9223372036854775808
+#define GERA_INT_MIN (-9223372036854775807 - 1)
 
 GeraAllocation* gera___rc_alloc(size_t size, GeraFreeHandler fh);
 void gera___rc_incr(GeraAllocation* a);
